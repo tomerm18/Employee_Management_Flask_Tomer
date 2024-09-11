ed90fc0457aff0931f9cc1e14fe84f148b0b335e
Tomer Magen <tomerm1998@gmail.com>
2024-09-11 23:34:23 +0300

    comm

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
-import os
+# import os
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://user:password@localhost/employee_management'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')
+# class Config:
+#     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+#     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql+pymysql://user:password@localhost/employee_management'
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+#     UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')
+
+
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.get('/hello')
+def get_hello():
+    return 'Hello, World!'
+app.run("0.0.0.0",5000)
